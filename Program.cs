a61fd60ca91c7fe456d1bd1716ec93d83a4ace72
ismail-smi <ismails.bsc@gmail.com>
2025-03-08 12:50:55 +0530

    Revert "My Method Added"
    This reverts commit ca89d6866278760241fc739285ad520c810d4ca0.

--- Program.cs
+++ Program.cs
@@ -4,8 +4,3 @@ Console.WriteLine("Welcome back again!!");
 Console.WriteLine("Welcome back again!!");
 Console.WriteLine("Welcome back again!!");
 Console.WriteLine("Welcome back again!!");
-Mymethod();
-
-void Mymethod() {
-    Console.WriteLine("MY METHOD");
-}
